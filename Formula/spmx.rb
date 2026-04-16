1169797709e16c1f5792e9855f9796625748b26f
macitch <petritvosha@outlook.com>
2026-04-16 20:39:36 +0200

    Update spmx.rb

--- Formula/spmx.rb
+++ Formula/spmx.rb
@@ -1,19 +1,23 @@
-class Spmx < Formula
-  desc "The dependency commands Swift Package Manager forgot to ship"
-  homepage "https://github.com/macitch/spmx"
-  url "https://github.com/macitch/spmx/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "6ac430f99bfdf9719640d59c6a065a45aec0a6f984a8cc406659bbe8b9137e92"
-  license "MIT"
-
-  depends_on xcode: ["16.0", :build]
-  depends_on :macos
-
+class Spmx < Formula                                                                                            
+   desc "The dependency commands Swift Package Manager forgot to ship"                                           
+   homepage "https://github.com/macitch/spmx"                                                                    
+   url "https://github.com/macitch/spmx/releases/download/v0.1.1/spmx-0.1.1-macos.tar.gz"                        
+   sha256 "13ed8aff1e0860ab0cdb51621e255e4589fb027c58c477d66fa79d6b974d9de5"
+   license "MIT"                                                                                                 
+   head "https://github.com/macitch/spmx.git", branch: "main"    
+                                                                                                                  
+   depends_on :macos                                             
+   depends_on xcode: ["16.0", :build] if build.head?                                                             
+                                                                  
   def install
-    system "swift", "build", "-c", "release", "--disable-sandbox"
-    bin.install ".build/release/spmx"
+    if build.head?
+      system "swift", "build", "-c", "release", "--disable-sandbox"
+      bin.install ".build/release/spmx"                                                                         
+    else
+      bin.install "spmx"                                                                                        
+    end                                                         
   end
-
-  test do
-    assert_match "0.1.0", shell_output("#{bin}/spmx --version")
-  end
+    test do
+      assert_match(/\d+\.\d+\.\d+/, shell_output("#{bin}/spmx --version"))
+  end                                                                                                           
 end
